364184efc1c6fc669c3249df6814cd847c62ebfc
Kwon <kdg5436@gmail.com>
2025-05-19 15:19:14 +0900

    refactor: FaqItem 컴포넌트 코드 정리 및 스타일 수정

--- src/components/faq/FaqItem.tsx
+++ src/components/faq/FaqItem.tsx
@@ -1,45 +1,3 @@
-// import { useState } from "react";
-
-// import ArrowDown from "../../assets/arror-down.svg";
-// import ArrowUp from "../../assets/arror-up.svg";
-
-// interface FaqItemProps {
-//   question: string;
-//   answer: string;
-// }
-
-// const FaqItem = ({ question, answer }: FaqItemProps) => {
-//   const [open, setOpen] = useState<boolean>(false);
-
-//   return (
-//     <div
-//       className="flex flex-col justify-center rounded-[30px] w-[900px] px-[50px] py-5 font-bold bg-[#345c5b] cursor-pointer"
-//       onClick={() => setOpen(!open)}
-//     >
-//       {/* 질문 */}
-//       <div
-//         className="flex justify-between gap-[40px] items-center cursor-pointer min-h-[40px]"
-//         onClick={() => setOpen(!open)}
-//       >
-//         <h2 className="text-2xl font-bold">{question}</h2>
-//         <img
-//           src={open ? ArrowUp : ArrowDown}
-//           alt={open ? "접기" : "펼치기"}
-//           className="w-5 h-2.5 inline"
-//         />
-//       </div>
-
-//       {/* 답 */}
-//       {open && (
-//         <p className="px-2 pt-5 pb-2 mt-2 text-lg text-start whitespace-pre-line font-normal transition-all duration-200">
-//           {answer}
-//         </p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FaqItem;
 import { useState, useRef, useLayoutEffect } from "react";
 
 import ArrowDown from "../../assets/arror-down.svg";
@@ -65,12 +23,12 @@ const FaqItem = ({ question, answer }: FaqItemProps) => {
 
   return (
     <div
-      className="flex flex-col justify-center rounded-[30px] w-[900px] px-[50px] py-5 font-bold bg-[#345c5b] transition-all duration-300 overflow-hidden cursor-pointer"
+      className="flex flex-col justify-center rounded-[30px] w-[900px] px-[50px] font-bold bg-[#345c5b] transition-all duration-300 overflow-hidden cursor-pointer"
       style={{ height: `${height}px` }}
       onClick={() => setOpen(!open)}
     >
       {/* 질문 */}
-      <div className="flex justify-between gap-[40px] items-center cursor-pointer">
+      <div className="flex justify-between items-center cursor-pointer">
         <h2 className="text-2xl font-bold">{question}</h2>
         <img
           src={open ? ArrowUp : ArrowDown}
@@ -80,11 +38,11 @@ const FaqItem = ({ question, answer }: FaqItemProps) => {
       </div>
 
       {/* 답 */}
-      <div className="mt-6">
+      <div>
         {open && (
           <p
             ref={answerRef}
-            className="px-2 pb-2 text-lg text-start whitespace-pre-line font-normal transition-all duration-200"
+            className="px-2 pb-2 text-lg text-start whitespace-pre-line font-normal transition-all duration-200 mt-[25px]"
           >
             {answer}
           </p>
